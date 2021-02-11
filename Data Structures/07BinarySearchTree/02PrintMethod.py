9bd50a08f89a6afdf4dcc1a6ecfe6b497e863907
kartik8460 <umeshgupta700@gmail.com>
2021-02-11 13:28:02 +0530

    BST -Insertion

---
+++ Data Structures/07BinarySearchTree/02PrintMethod.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init(self, key):
+        self.left = None
+        self.right = None
+        self.key = key
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def get_root(self):
+        return self.root
+
+    def is_empty(self):
+        if self.root is None:
+            return True
+        else:
+            return False
+
+    def print_tree(self):
+        if self.is_empty():
+            print("Tree is Empty")
+            return
+        root = self.get_root()
+        def print_tree_helper(node):
+
+
+if __name__ == "__main__":
+    tree = BinarySearchTree()
